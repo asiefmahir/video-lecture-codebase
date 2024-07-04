cca9fde132536561d33f0cf8d2012c962b601106
Asief Mahir <asiefmahir1@gmail.com>
2024-07-05 00:40:05 +0600

    feat: Learn about Reusable Components in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import BioData from "./components/BioData";
+
+const bioData = [
+	{
+		name: "SR Setu",
+		email: "srsetu@gmail.com",
+		phone: "+245456452132452",
+		github: "github.com/srsetu",
+		skills: ["JS", "WP", "CSS", "REACT", "Next"],
+		interests: ["Chess", "Football", "Entrepreneurship"],
+		socialLinks: [
+			{ platformName: "FB", handle: "fb/srsetu" },
+			{ platformName: "Twitter", handle: "x/srsetu" },
+			{ platformName: "LinkedIn", handle: "linkedin/In/srsetu" },
+		],
+	},
+	{
+		name: "Mahir Asief",
+		email: "asiefmahir1@gmail.com",
+		phone: "+468454565456",
+		github: "github.com/asiefmahir",
+		skills: ["JS", "REACT", "Next"],
+		interests: ["Chess", "Football"],
+		socialLinks: [
+			{ platformName: "FB", handle: "fb/asiefmahir" },
+			{ platformName: "Twitter", handle: "x/asiefmahir" },
+			{ platformName: "LinkedIn", handle: "linkedin/In/asiefmahir" },
+		],
+	},
+	{
+		name: "Sumit Shaha",
+		email: "sumitshaha@gmail.com",
+		phone: "+468454565456",
+		github: "github.com/sumitshaha",
+		skills: ["JS", "REACT", "Next"],
+		interests: ["Chess", "Football"],
+		socialLinks: [
+			{ platformName: "FB", handle: "fb/sumitshaha" },
+			{ platformName: "Twitter", handle: "x/sumitshaha" },
+			{ platformName: "LinkedIn", handle: "linkedin/In/sumitshaha" },
+		],
+	},
+];
+
 function App() {
-	console.log("I am called");
-	return <div className="App">Hello React</div>;
+	// console.log("I am called");
+	return (
+		<div className="App">
+			{bioData.map((singleBioData) => (
+				<>
+					<BioData
+						name={singleBioData.name}
+						email={singleBioData.email}
+						phone={singleBioData.phone}
+						github={singleBioData.github}
+						skills={singleBioData.skills}
+						interests={singleBioData.interests}
+						socialLinks={singleBioData.socialLinks}
+					/>
+					<hr />
+				</>
+			))}
+		</div>
+	);
 }
+
+// BioData({p})
 
 /**
  * A component must fullfil 3 criteria:
