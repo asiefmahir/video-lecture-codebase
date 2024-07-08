e436d7455680253e5760274adcb8815925fd8b0c
Asief Mahir <asiefmahir1@gmail.com>
2024-07-09 02:47:52 +0600

    feat: Create a trello clone with dnd feature

--- src/components/CounterAppTitle.jsx
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-
-const CounterAppTitle = memo(({ value }) => {
-	console.log("I am CounterAppTitle");
-	return <p>The value of the Counter is {value}</p>;
-});
-
-export default CounterAppTitle;
